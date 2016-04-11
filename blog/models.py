1d5cf357e6a4e55a560ab0b7fcdc41234b173543
wuji zhang <wuji zhang>
2016-04-11 21:51:09 +0800

    3

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,9 @@ class Post(models.Model):
     published_date=models.DateTimeField(blank=True,null=True)
 
     def publish(self):
-        self.publish_date=timezone.now()
+        self.published_date=timezone.now()#只改了日期，时间改不了
         self.save()
+
 
     def __str__(self):
         return self.title
